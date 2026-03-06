f56c387016567bef9e42592fd274201ff768743b
Alberto Cabeza <venezuelawiro2@gmail.com>
2026-03-06 14:34:10 -0400

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.nav-links');
-
-menu.addEventListener('click', function() {
-    menuLinks.classList.toggle('active');
-    
-    // Animación simple del botón (opcional)
-    menu.classList.toggle('is-active');
-});
